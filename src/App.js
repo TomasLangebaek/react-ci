7c72df2cfa9d78178bb0c879124be6a81a68052a
Tomas Langebaek <t.langebaek@uniandes.edu.co>
2021-03-19 18:24:24 -0500

    corrections

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
-
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { useApi } from './useApi';
 import LoadingSpinner from './LoadingSpinner';
 import ErrorMessage from './ErrorMessage';
 import PokemonList from './PokemonList';
 
-//const unused = {};
-const mapResults = (({ results }) => results.map(({ url, name }) => ({
-  url,
-  name,
-  id: parseInt(url.match(/\/(\d+)\//)[1])
-})));
+const mapResults = ({ results }) =>
+  results.map(({ url, name }) => ({
+    url,
+    name,
+    id: parseInt(url.match(/\/(\d+)\//)[1]),
+  }));
 
 function App() {
-  const { data: pokemonList, error, isLoading } = useApi('https://pokeapi.co/api/v2/pokemon/?limit=784', mapResults);
+  const { data: pokemonList, error, isLoading } = useApi(
+    'https://pokeapi.co/api/v2/pokemon/?limit=784',
+    mapResults
+  );
   if (isLoading) {
     return <LoadingSpinner />;
   }
@@ -21,11 +23,10 @@ function App() {
     return <ErrorMessage error={error} />;
   }
 
-
   return (
     <Router>
       <Switch>
-        <Route exact path="/">
+        <Route exact path='/'>
           <PokemonList pokemonList={pokemonList} />
         </Route>
       </Switch>
--- src/PokemonList.js
+++ src/PokemonList.js
@@ -5,15 +5,17 @@ import { Link } from 'react-router-dom';
 const PokemonList = ({ pokemonList }) => {
   return (
     <>
-      <h1 className="title">Pokemons</h1>
-      <div className="list-container">
+      <h1 className='title'>Pokemons</h1>
+      <div className='list-container'>
         {pokemonList.map(({ id, name }) => (
-          <Link key={id} className="list-item" style={{ backgroundImage: `url(${`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`})` }}>
-            <div
-              className="list-item-name"
-            >
-              {name}
-            </div>
+          <Link
+            key={id}
+            className='list-item'
+            style={{
+              backgroundImage: `url(${`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`})`,
+            }}
+          >
+            <div className='list-item-name'>{name}</div>
           </Link>
         ))}
       </div>
@@ -22,7 +24,7 @@ const PokemonList = ({ pokemonList }) => {
 };
 
 PokemonList.PropTypes = {
-  pokemonList : PropTypes.array.isRequired
+  pokemonList: PropTypes.array.isRequired,
 };
 
 export default PokemonList;
